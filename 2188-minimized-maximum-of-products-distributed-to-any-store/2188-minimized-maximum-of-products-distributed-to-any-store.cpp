3cb05803dc9963a1c06257c0fd83b5cf937a8015
dohoon0122 <57519901+dohoon0122@users.noreply.github.com>
2024-11-14 12:29:41 +0900

    Time: 20 ms (95.51%), Space: 87.3 MB (34.13%) - LeetHub

--- 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,49 +1,34 @@
 class Solution {
 public:
     int minimizedMaximum(int n, vector<int>& quantities) {
-        int m = quantities.size();
-        int remain = n - m;
-        auto customCompareLambda = [](pair<int, int>& a, pair<int, int>& b){
-            return (long long)a.first*b.second < (long long)a.second*b.first;
-        };
-        priority_queue<pair<int,int>, vector<pair<int, int>>, decltype(customCompareLambda)> store; 
-
+        int left = 1;
+        int right = INT_MIN;
         for(int q: quantities){
-            store.push(make_pair(q, 1));
+            right = max(right, q);
         }
 
-        while(remain){
-            auto curr = store.top();
-            store.pop();
-            curr.second++;
-
-            store.push(curr);
-            remain--;
+        int ans;
+        while(left<right){
+            int mid = left + (right-left)/2;
+            if(isPossible(n, mid, quantities)){
+                right = mid;
+                ans = mid;
+            }
+            else{
+                left = mid + 1;
+            }
         }
 
+        return left;
+    }
 
-        return ceil((double)store.top().first / store.top().second);
+    bool isPossible(int n, int x, vector<int>& quantities){
+        int i = 0;
+        for(int q: quantities){
+            i += q/x;
+            if(q%x) i++;
+            if(i>n) return false;
+        }
+        return true;
     }
 };
-
-/*
-n 개 상점, m type product
-일단 m개 상점에 다 넣기
-
-15 10 10 0 0 0 0
-그 다음 distribute.
-max_heap 뽑아낸뒤, top 절반, 다시 넣고, remain 0 될때까지
-7 10 10 8 0 0 0
-7 5 10 8 5 0 0
-7 5 5 8 5 5 0
-7 5 5 4 5 5 4 -> 반례
-
--------
-=> top 절반이 아니라, 같은type의 max 갯수로 하면 되는듯?
-15 10 10  remain 4
-8 10 10 remain 3
-8 5 5 remain 1
-15, 1, 
-
-
-*/
