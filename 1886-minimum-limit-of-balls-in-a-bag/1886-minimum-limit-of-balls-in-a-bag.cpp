5737ca64774866d7fcc55acf3de614aee98d0642
dohoon0122 <57519901+dohoon0122@users.noreply.github.com>
2024-12-08 01:18:36 +0900

    Time: 45 ms (30%), Space: 59.8 MB (15.54%) - LeetHub

---
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int n = nums.size();
+        int bags = n + maxOperations;
+
+        int left = 1, right = INT_MIN;
+        for(int b: nums){
+            right = max(right, b);
+        }
+
+        int ans;
+        while(left<right){
+            int mid = left + (right-left)/2;
+            if(isPossible(nums, mid, bags)){
+                right = mid;
+                ans = mid;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    bool isPossible(vector<int>& nums, int dist, int bags){
+        int bag = 0;
+        for(auto ball: nums){
+            bag += ball/dist;
+            if(ball%dist) bag++;
+            if(bag>bags) return false;
+        }
+        return true;
+    }
+};
