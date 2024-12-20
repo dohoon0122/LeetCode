b4b5521d9fb88a6d477578091c18a31eabe12a7b
dohoon0122 <57519901+dohoon0122@users.noreply.github.com>
2024-12-20 17:34:51 +0900

    Time: 381 ms (20.45%), Space: 92.2 MB (84.96%) - LeetHub

---
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        int N = classes.size();
+
+        auto comp = [](const pair<int, int>& a, const pair<int, int>& b){
+            double incA = (double)(a.first + 1) / (a.second + 1) - (double)a.first / a.second;
+            double incB = (double)(b.first + 1) / (b.second + 1) - (double)b.first / b.second;
+
+            return incA < incB;
+        };
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq;
+
+        double ans = 0;
+        for(const auto& cls: classes){
+            pq.push({cls[0], cls[1]});
+            ans += (double)cls[0]/cls[1];
+        }
+
+        while(extraStudents--){
+            auto [pass, total] = pq.top();
+            pq.pop();
+            ans -= (double)pass/total;
+
+            pq.push({pass+1, total+1});
+            ans += (double)(pass+1)/(total+1);
+        }
+
+        return ans/N;
+    }
+};
