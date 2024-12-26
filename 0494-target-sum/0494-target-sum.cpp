02f6fa3d3f81c5f2829318dd9165735c0f68b6fb
dohoon0122 <57519901+dohoon0122@users.noreply.github.com>
2024-12-26 16:00:01 +0900

    Time: 6 ms (74.31%), Space: 15.1 MB (47.4%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    int ans;
-
     int findTargetSumWays(vector<int>& nums, int target) {
         int N = nums.size();
         int total = 0;
         int zero_cnt = 0;
+        sort(nums.begin(), nums.end());
         for(int number : nums){
             total += number;
             if(number == 0) zero_cnt++;
         }
-
-        sort(nums.begin(), nums.end());
-
         int minus = total - target;
         if(minus%2) return 0;
         minus /= 2;
+        if(minus == 0) return 1*(1<<zero_cnt);
+        if(minus<0) return 0;
 
-        ans = 0;
-        backtrack(zero_cnt, N, nums, 0, minus);
-
-        cout<<minus<<" "<<zero_cnt<<"\n";
-
-        return ans*(1<<zero_cnt);
-    }
-
-    void backtrack(int idx, int N, vector<int>& nums, int curr, int target){
-        if(curr > target) return;
-        if(curr == target){
-            ans++;
-            return;
+        vector<vector<int>> dp(N-zero_cnt, vector<int>(minus+1));
+        dp[0][0] = 1;
+        if(nums[zero_cnt]<=minus) dp[0][nums[zero_cnt]] = 1;
+        for(int i=zero_cnt; i<N-1; i++){
+            for(int j=0; j<=minus; j++){
+                dp[i-zero_cnt+1][j]+=dp[i-zero_cnt][j];
+                if(j+nums[i+1]<=minus) dp[i-zero_cnt+1][j+nums[i+1]]+=dp[i-zero_cnt][j];
+            }
         }
-        if(idx >= N) return;
 
-        backtrack(idx+1, N, nums, curr + nums[idx], target);
-        backtrack(idx+1, N, nums, curr, target);
+
+        return dp[N-zero_cnt-1][minus]*(1<<zero_cnt);
     }
+
 };
+/*
+  0 1 2 
+0 1 1 1 
+1 1 2 2
+2 0 1 2 
+*/
