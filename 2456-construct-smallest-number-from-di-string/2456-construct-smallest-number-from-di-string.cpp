31ed244faec312a28a217c461bf337e6bed5bcaf
dohoon0122 <57519901+dohoon0122@users.noreply.github.com>
2025-02-19 00:05:19 +0900

    Time: 0 ms (100%), Space: 8.1 MB (49.49%) - LeetHub

---
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string smallestNumber(string pattern) {
+        string ans = "";
+        stack<int> st;
+
+        for(int i=0; i<=pattern.size(); i++){
+            st.push(i+1);
+            if(i==pattern.size() || pattern[i] == 'I'){
+                while(!st.empty()){
+                    ans+=(st.top() + '0');
+                    st.pop();
+                }
+            }
+        }
+
+        return ans;
+    }
+};
